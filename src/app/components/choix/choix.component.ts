323ec00c24487223816c371743915df5bb53c855
mounaredissi <mounaaredissi@gmail.Com>
2021-07-15 23:57:00 +0100

    3rd version

--- src/app/components/choix/choix.component.ts
+++ src/app/components/choix/choix.component.ts
@@ -1,21 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { TypeEnum } from 'src/app/sign-up/sign-up.component';
 
 @Component({
   selector: 'app-choix',
   templateUrl: './choix.component.html',
-  styleUrls: ['./choix.component.css']
+  styleUrls: ['./choix.component.css'],
 })
 export class ChoixComponent implements OnInit {
-choice:boolean=false;
-  constructor() { }
+  choice: boolean = false;
+  routeType = TypeEnum;
 
-  ngOnInit(): void {
-  }
-  public click (){
-    this.choice=true;
+  constructor() {}
 
+  ngOnInit(): void {}
+  public click() {
+    this.choice = true;
   }
-  public get  (){
+  public get() {
     return this.choice;
   }
 }
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,70 +1,100 @@
 import { Component, OnInit } from '@angular/core';
 import { SignUpService } from '../services/sign-up.service';
-import { ChoixComponent } from '../components/choix/choix.component';
 import {FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
+import { Route } from '@angular/compiler/src/core';
+import { ActivatedRoute } from '@angular/router';
 interface piece {
   value: string;
   viewValue: string;
+}
+export enum TypeEnum {
+  CANDIDAT,
+  ENTREPRISE,
 }
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  styleUrls: ['./sign-up.component.css'],
 })
 export class SignUpComponent implements OnInit {
-  services:any;
-  form1:FormGroup;
-  forms:FormGroup[];
-  var:boolean=false;
-  selectedServices:SignUpService[]=[];
-  page =0;
+  type: number;
+  typeEnum = TypeEnum;
+  services: any;
+  formClient: FormGroup;
+  formEntreprise: FormGroup;
+
+  forms: FormGroup[];
+  var: boolean = false;
+  selectedServices: SignUpService[] = [];
+  page = 0;
   piece: piece[] = [
-    {value: 'CIN', viewValue: 'CIN'},
-    {value: 'passeport', viewValue: 'passeport'},
-    {value: 'carte séjour', viewValue: 'carte séjour'}
+    { value: 'CIN', viewValue: 'CIN' },
+    { value: 'passeport', viewValue: 'passeport' },
+    { value: 'carte séjour', viewValue: 'carte séjour' },
   ];
-  documents:any;
+  documents: any;
   signUpForm = {
     firstName: '',
     lastName: '',
-    email: ''
+    email: '',
   };
   submitted = false;
-  constructor(private inscriptionService:SignUpService,  private _formBuilder: FormBuilder, choix:ChoixComponent )
-  { this.form1 = this._formBuilder.group({
-    name: ['', Validators.required],
-    lastname:['', Validators.required],
-    numtel:['',Validators.required],
-    adresseemail:['',Validators.required],
-    pjControl:['',Validators.required],
-   dateexp:['', Validators.required],
-   
- });
-   this.var=choix.get();
-    this.forms = [this.form1];
-   }
+  constructor(
+    private inscriptionService: SignUpService,
+    private _formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+  ) {
+    this.type = this.route.snapshot.params.type;
+    this.formClient = this._formBuilder.group({
+      name: ['', Validators.required],
+      lastname: ['', Validators.required],
+      numtel: ['', Validators.required],
+      adresseemail: ['', Validators.required],
+      pjControl: ['', Validators.required],
+      dateexp: ['', Validators.required],
+    });
+    this.formEntreprise = this._formBuilder.group({
+      name: ['', Validators.required],
+      lastname:['', Validators.required],
+      numtel:['',Validators.required],
+      adresseemail:['',Validators.required],
+      pjControl:['',Validators.required],
+     dateexp:['', Validators.required],
+     numpiece:['', Validators.required],
+     adresse:['', Validators.required],
+     nameprof :['', Validators.required],
+     lastnameprof:['', Validators.required],
+     numtelprof:['', Validators.required],
+     adresseprof:['', Validators.required],
+     adressemailprof:['', Validators.required],
+
+   });
+
+    this.var = true;
+    this.forms = [this.formClient, this.formEntreprise];
+  }
 
   ngOnInit(): void {
-    this.inscriptionService.getAllServicesList()
-    .subscribe(
-      data => {
+    this.inscriptionService.getAllServicesList().subscribe(
+      (data) => {
         this.services = data;
         console.log(data);
       },
-      error => {
+      (error) => {
         console.log(error);
-      });
+      }
+    );
 
-    this.inscriptionService.getAllDocumentList()
-      .subscribe(
-        data => {
-          this.documents = data;
-          console.log(data);
-        },
-        error => {
-          console.log(error);
-        });
+    this.inscriptionService.getAllDocumentList().subscribe(
+      (data) => {
+        this.documents = data;
+        console.log(data);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
   public next() {
@@ -72,7 +102,7 @@ export class SignUpComponent implements OnInit {
     this.page < 3 && this.page++;
   }
 
-  public adddoc(doc:any){
+  public adddoc(doc: any) {
     console.log(doc);
   }
 
@@ -80,19 +110,30 @@ export class SignUpComponent implements OnInit {
     this.page -= 1;
   }
   public getFinalPrice(): number {
-    return this.selectedServices.reduce((acc:any, currentService:any) => acc + currentService.price, 0)
+    return this.selectedServices.reduce(
+      (acc: any, currentService: any) => acc + currentService.price,
+      0
+    );
   }
 
   public addService(service: SignUpService) {
     console.log(service);
     const index = this.selectedServices.indexOf(service);
-    if(index > -1) {
+    if (index > -1) {
       this.selectedServices.splice(index, 1);
     } else {
       this.selectedServices.push(service);
     }
   }
- /* submit() {
+  /* submit() {
+
+{
+  ClientId: this.formCandidat.value.id,
+
+}
+
+
+
     debugger;
   //  const { name, lastname, numtel, adresseemail, pjControl } = this.form1.value;
 
